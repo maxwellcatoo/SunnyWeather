1e2a6e9f7f6f618898ca1ce6a9e26d52d8011388
LAPTOP-U5ON4HSK\maxwellcatoo <1507499759@qq.com>
2022-04-12 00:24:02 +0800

    日常学习安卓开发

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -2,11 +2,49 @@ package com.sunnyweather.android.ui.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.sunnyweather.android.R
+import com.sunnyweather.android.logic.model.Weather
+import kotlinx.android.synthetic.main.now.*
 
 class WeatherActivity : AppCompatActivity() {
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(WeatherViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+
+        if (viewModel.locationLng.isEmpty()) {
+            viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
+        }
+        if (viewModel.locationLat.isEmpty()) {
+            viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
+        }
+        if (viewModel.placeName.isNotEmpty()) {
+            viewModel.placeName = intent.getStringExtra("place_name") ?: ""
+        }
+        viewModel.weatherViewModel.observe(this, Observer{ result ->
+            val weather = result.getOrNull()
+            if (weather != null) {
+                showWeatherInfo(weather)
+            } else {
+                Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace() // todo 没看明白这一行干啥的
+            }
+        })
+        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+    }
+
+    private fun showWeatherInfo(weather: Weather) {
+        placeName.text = viewModel.placeName
+        val realtime = weather.realtime
+        val daily = weather.daily
+        // 填充now.xml布局中的数据
+        val currentTempText = "${realtime.temperature.toInt()} °C"
+        // 。。。
     }
 }
